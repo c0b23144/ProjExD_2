bf6bddcdffec121ecd0afe7a01e68e1d3bb5f447
Baru-chan <c0b23144be@edu.teu.ac.jp>
2024-04-23 14:56:38 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
@@ -12,6 +13,20 @@ DELTA = {  #移動量辞書 (押下キー)
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(obj_rct) -> tuple[bool, bool]:
+    """
+    こうかとんRect または,爆弾Rectの画面内外判定用の関数
+    引数: こうかとんRect,または,爆弾Rect
+    戻り値: 横方向判定結果, 縦方向判定結果 (True:画面内/ False:画面外)
+    """
+    yoko, tate = True, True
+    if obj_rct.right < 0 or WIDTH < obj_rct.left:
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.top:
+        tate = False
+    return yoko, tate
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -20,6 +35,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    bd_img = pg.Surface((20, 20))
+    bd_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()
+    bd_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -35,9 +57,27 @@ def main():
             if key_lst[k]:
                sum_mv[0] += v[0]
                sum_mv[1] += v[1] 
+       # if key_lst[pg.K_UP]:
+        #    sum_mv[1] -= 5
+        #if key_lst[pg.K_DOWN]:
+        #    sum_mv[1] += 5
+        #if key_lst[pg.K_LEFT]:
+         #   sum_mv[0] -= 5
+        #if key_lst[pg.K_RIGHT]:
+         #   sum_mv[0] += 5
         
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        bd_rct.move_ip(vx, vy)
+        screen.blit(bd_img, bd_rct)
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
